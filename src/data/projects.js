9f9ef25ae4829f6f77601928d9485331bce7ce7e
AnujSharma141 <19ucc109@lnmiit.ac.in>
2021-07-25 09:04:47 +0530

    loader and transit added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,36 +1,40 @@
 const projects = [
-    {
-      text: "stonk",
-      url: "https://www.gatsbyjs.com/docs/tutorial/",
-      description:
-        "command line tool to monitor indian stocks data stocks watchlists.",
-      color: "#E95800",
-      button: "#E95800",
-    },
-    {
-      text: "reactsearch",
-      url: "https://www.gatsbyjs.com/docs/how-to/",
-      description:
-        "NPM package to create React search input component.",
-      color: "#1099A8",
-      button: "#E95800",
-    },
-    {
-      text: "pharmacy",
-      url: "https://www.gatsbyjs.com/docs/reference/",
-      description:
-        "dashboard to manage the workflow of a Pharmacy Store.",
-      color: "#BC027F",
-      button: "#E95800",
-    },
-    {
-      text: "kaizen",
-      url: "https://www.gatsbyjs.com/docs/conceptual/",
-      description:
-        "anime data API made over a GraphQL server by scraping web.",
-      color: "#0D96F2",
-      button: "#E95800",
-    },
-   ]
+  {
+    text: "stonk",
+    url: "https://www.gatsbyjs.com/docs/tutorial/",
+    image: "https://i.ibb.co/5xJ18xT/Image-2.png",
+    description:
+      "command line tool to monitor indian stocks data stocks watchlists.",
+    color: "#EAFEF9",
+    button: "#32A37A",
+  },
+  {
+    text: "reactsearch",
+    url: "https://www.gatsbyjs.com/docs/how-to/",
+    description:
+      "NPM package to create React search input component.",
+    image: "https://camo.githubusercontent.com/2b6ec54a9a33178744c381e0c082d61577b09618bdebfa7484bc9421824dc545/68747470733a2f2f692e6962622e636f2f784446663742632f416e696d6174696f6e2e676966",
+    color: "#FFE3F4",
+    button: "#AD29A4",
+  },
+  {
+    text: "pharmacy",
+    url: "https://www.gatsbyjs.com/docs/reference/",
+    description:
+      "dashboard to manage the workflow of a Pharmacy Store.",
+    image: "https://i.ibb.co/5xJ18xT/Image-2.png",
+    color: "#E8EBFF",
+    button: "#2F5B8D",
+  },
+  {
+    text: "kaizen",
+    url: "https://www.gatsbyjs.com/docs/conceptual/",
+    description:
+      "anime data API made over a GraphQL server by scraping web.",
+    image: "https://i.ibb.co/5xJ18xT/Image-2.png",  
+    color: "#FFE3DC",
+    button: "#C97726",
+  },
+ ]
 
-   module.exports = projects   
+ module.exports = projects   
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
 import * as React from "react"
+import { useState } from "react"
+import Switch from "react-switch"
+import Fade from 'react-reveal/Fade'
+import Rotate from 'react-reveal/Rotate'
+import Swing from 'react-reveal/Swing';
+
 
 import projects from "../data/projects"
 import social from "../data/social"
@@ -6,84 +12,118 @@ import about from '../data/about'
 
 import '../style/base.sass'
 
+import logo from "../images/logo.svg"
 import wave from "../images/waving-hand_1f44b.png"
 import laptop from "../images/laptop_1f4bb.png"
 import ruler from "../images/straight-ruler_1f4cf.png"
 import mail from "../images/envelope_2709-fe0f.png"
 import heart from "../images/red-heart_2764-fe0f.png"
 
+const Loader = () => {
+    return(
+        <div className="loader">
+            <img src={logo} className="loader-img" alt="" />    
+        </div>
+    )
+}
+
 // markup
 const IndexPage = () => {
+  const [dark, setDark] = useState(false) 
+  const [load, setLoad] = useState(true) 
+  setTimeout(() => {
+      setLoad(false)
+  }, 1700); 
+  const toggleSwitch = () => setDark(!dark)   
+  dark? document.querySelector("body").className = "dark"
+  : document.querySelector("body").className = "light"
+
   return (
-    <main >
+    <main>
       <title>Anuj Sharma</title>
-      <div classname="land">
-
-        <div classname="graphic">
-          <div classname="graphic-elipse"><img src={laptop} alt="" /></div>
-          <div classname="graphic-label">Javascript Developer</div>
-          <div classname="graphic-label">UI Designer</div>
-          <div classname="graphic-elipse"><img src={ruler} alt="" /></div>
+      {load?<Loader />:
+      <>
+      <div className="switch">
+      <Switch activeBoxShadow="0 0 0 0" offColor="#DBDBDB" width={28} handleDiameter={13} height={16} onChange={toggleSwitch} uncheckedIcon={null} checkedIcon={null} checked={dark} />
+      </div>
+      <div className={dark?"land dark-land": "land"}>
+      
+      <Rotate top left cascade>
+        <div className={dark?"graphic dark-graphic": "graphic"}>
+          <div className={dark?"graphic-elipse elipse-blue dark-elipse-blue": "graphic-elipse elipse-blue"}>
+              <img className="graphic-laptop" src={laptop} alt="" /></div>
+          <div className={dark?"graphic-label label-orange dark-label-orange": "graphic-label label-orange"}>Javascript Developer</div>
+          <div className={dark?"graphic-label label-green dark-label-green": "graphic-label label-green"}>UI Designer</div>
+          <div className={dark?"graphic-elipse elipse-pink dark-elipse-pink": "graphic-elipse elipse-pink"}><img className="graphic-ruler" src={ruler} alt="" /></div>
         </div>
+        </Rotate>
 
-        <div classname="textbox">
-        <img src={wave} alt="" />
-        <div classname="textbox-wrap">
-          <p classname="textbox-text">Hey, I am</p>
-          <h3 classname="textbox-title">Anuj Sharma</h3>
+        <Fade top cascade>
+        <div className={dark?"textbox dark-textbox": "textbox"}>
+        <Swing>    
+        <img className="textbox-img" src={wave} alt="" />
+        </Swing>
+        <div className="textbox-wrap">
+          <p className={dark?"textbox-text dark-text": "textbox-text"}>Hey, I am</p>
+          <h3 className={dark?"textbox-title dark-title": "textbox-title"}>Anuj Sharma</h3>
         </div>
       </div>
+      </Fade>
       
     </div>
 
 
-    <div classname="about">
-    <h2 classname="about-title">About</h2>
-    <p classname="about-text">{about}</p>
+    <div className={dark?"about dark-about": "about"}>
+    <h2 className="about-title">About</h2>
+    <p className="about-text">{about}</p>
     </div>
     
-    <div classname="projects">
-    <h2 classname="projects-title">Projects</h2>
+    <div className={dark?"project dark-project": "project"}>
+    <h2 className={dark?"project-heading dark-title": "project-heading"}>Projects</h2>
     {projects.map(item=>{
       return<>
-      <div classname="project-item">
-        <img classname="project-img" src="" alt="" />
-        <div classname="project-counter">
-          <h4 classname="project-title">{item.text}</h4>
-          <p classname="project-text">{item.description}</p>
-          <a classname="project-button" href="">view</a>
+      <div className="project-item" style={{background: dark?item.button:item.color}}>
+        <img className="project-img" src={item.image} alt="" />
+        <div className="project-counter" >
+          <h4 className={dark?"project-title dark-title": "project-title"}>{item.text}</h4>
+          <p className={dark?"project-text dark-text": "project-text"}>{item.description}</p>
+          <a className={dark?"project-button dark-context": "project-button"} style={{background: dark?item.color:item.button}} href="">view</a>
         </div>
       </div>
       </>
     })}
     </div>
 
-    <div classname="contact">
-    <img classname="contact-img" src={mail} alt="" />
-    <h2 classname="contact-title">connect with me!</h2>
-    <p classname="contact-text">feel free to drop a mail </p>
-    <div classname="contact-social">
-    <a classname="contact-mail" href="">email</a>
+    <div className={dark?"contact dark-contact": "contact"}>
+    <img className="contact-img" src={mail} alt="" />
+    <h2 className={dark?"contact-title dark-title": "contact-title"}>connect with me!</h2>
+    <p className="contact-text">feel free to drop a mail </p>
+    <div className="contact-social">
+    <a className="contact-mail" href="">email</a>
+    <div>
     {social.map(item =>{
       return(
         <>
-        <a classname="contact-social-link" href={item.link}>
+        <a className="contact-social-link" href={item.link}>
           /
-          <img classname="contact-social-icon" src={item.icon} alt="" />
+          <img className="contact-social-icon" src={item.icon} alt="" />
         </a> 
         </>
       )
     })}
     </div>
     </div>
+    </div>
 
-    <p classname="credit">
-    <span classname="credit-text">made with</span>
-    <img classname="credit-icon" src={heart} alt="" />
-    <span classname="credit-text">by</span>
-    <span classname="credit-name">Anuj Sharma</span>
+    <p className="credit">
+    <span className="credit-text">made with</span>
+    <img className="credit-icon" src={heart} alt="" />
+    <span className="credit-text">by</span>
+    <span className={dark?"credit-name dark-credit-name": "credit-name"}>Anuj Sharma</span>
     </p>
 
+      </>}
+      
     </main>
   )
 }
