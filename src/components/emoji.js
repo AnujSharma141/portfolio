ce0e593883f60e7abcb9d98272a592cab450db72
Anuj Sharma <19ucc109@lnmiit.ac.in>
2022-02-01 23:55:02 +0530

    update: mobile viewports!

--- src/components/emoji.js
+++ src/components/emoji.js
@@ -4,7 +4,7 @@ import '../style/base.sass'
 const Emoji = props => {
     return(
         <>
-          <img style={{width: '1.5vw', height: '1.5vw', marginLeft: '0.4vw', marginRight:'0.4vw' , marginBottom:'-0.365vw'}} src={props.src} alt="" />  
+          <img classname={props.classname} style={{width: '1.5vw', height: '1.5vw', marginLeft: '0.4vw', marginRight:'0.4vw' , marginBottom:'-0.365vw'}} src={props.src} alt="" />  
         </>
     )
 }
