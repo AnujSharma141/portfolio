cda8f56b919beecb8b56fc6d13075da10ed27086
Anuj Sharma <19ucc109@lnmiit.ac.in>
2022-02-17 05:10:13 +0530

    update: work exp!

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,8 @@ import mail from "../images/envelope_2709-fe0f.png"
 import minecraft from '../images/Image 1.png'
 import heart from "../images/red-heart_2764-fe0f.png"
 import geek from "../images/man-technologist_1f468-200d-1f4bb.png"
+import student from "../images/man-student_1f468-200d-1f393.png"
+import gear from "../images/gear_2699-fe0f.png"
 
 
 
@@ -28,7 +30,7 @@ const IndexPage = () => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
-      <title>Anuj Sharma</title>
+      <title>anuj sharma</title>
       
       <div className="switch">
       <Switch activeBoxShadow="0 0 0 0" offColor="#DBDBDB" width={28} handleDiameter={13} height={16} onChange={toggleSwitch} uncheckedIcon={null} checkedIcon={null} checked={dark} />
@@ -40,30 +42,36 @@ const IndexPage = () => {
         <div className="text-message">heyyyy <Emoji src={wave} /></div>        
         <div className="textbox-wrap">
           <p className={dark?"textbox-text dark-text": "textbox-text"}>im
-          <span className={dark?"textbox-title dark-title": "textbox-title"}>Anuj Sharma</span>
-          i do  </p>
+          <span className={dark?"textbox-title dark-title": "textbox-title"}>anuj sharma</span>
+          </p>
         </div>
-
-      <div className="textbox-labels">
-      <div className={dark?"textbox-label label-orange dark-label-orange": "textbox-label label-orange"}>Javascript Dev</div>
+<h2 className={dark?"work-heading dark-title": "work-heading"}>a tech intern @ <a target="_blank" className={dark?"link-work dark-about-text": "link-work"} href="https://www.quidich.com/" > Quidich</a> <Emoji src={gear} /></h2>
+    
       
-      <div className={dark?"textbox-label label-green dark-label-green": "textbox-label label-green"}>UI Design</div>
-      </div>  
           
      
       
     </div>
 
+    <div className={dark?"work dark-about": "work"}>
 
+      <div className="textbox-labels"> i am a 
+      <div className={dark?"textbox-label label-orange dark-label-orange": "textbox-label label-orange"}>Javascript Developer</div>
+      and
+      <div className={dark?"textbox-label label-green dark-label-green": "textbox-label label-green"}>UI Designer</div>
+      </div>  
+    </div>
+    
     <div className={dark?"about dark-about": "about"}>
     <p className={dark?"about-text dark-about-text": "about-text"}>
-     im in my prefinal year studying<br /> 
-      <span className={dark?"about-text dark-about-text": "about-text-black"}>communication and computer engineering.</span> <br />
+     im in my prefinal year studying <Emoji src={student} /><br /> 
+      <span className={dark?"about-text dark-about-text": "about-text-black"}>communication and computer engineering . </span> <br />
      i like to read about web tech and i absolutely <Emoji src={heart} /> javascript <br />
      also i play <Emoji src={minecraft} /> or watch some anime when free ;p 
     </p>
     </div>
     
+
     <div className={dark?"project dark-project": "project"}>
     <h2 className={dark?"project-heading dark-title": "project-heading"}>some of my projects <Emoji src={geek} /></h2>
     {projects.map(item=>{
