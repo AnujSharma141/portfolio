3af86cf1572ea1e02ddaa5bb571426052a4ac9ef
Anuj Sharma <19ucc109@lnmiit.ac.in>
2023-08-08 17:11:03 +0530

    update: projects!

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,5 @@
 const projects = [
   {
-    text: "kaizen",
-    url: "https://kaizen-api-ju41.onrender.com/",
-    description:
-      "anime data API built over graphQL",
-    image: "https://i.ibb.co/5xJ18xT/Image-2.png",  
-    color: "#FFE3DC",
-    button: "#C97726",
-  },
-  {
-    text: "stonk",
-    url: "https://stonk.js.org/",
-    image: "https://i.ibb.co/kK1Hh1L/Group-2.png",
-    description:
-      "cli tool to monitor indian stocks",
-    color: "#EAFEF9",
-    button: "#32A37A",
-  },
-  {
     text: "reactsearch",
     url: "https://reactsearch.js.org/",
     description:
@@ -35,7 +17,33 @@ const projects = [
     color: "#E8EBFF",
     button: "#2F5B8D",
   },
-  
+  {
+    text: "lore",
+    url: "https://lorechatapp.web.app/",
+    description:
+      "web based chat application",
+    image: "https://i.ibb.co/VmsCrhd/Image-3.png",
+    color: "#E8EBFF",
+    button: "#2F5B8D",
+  },
+  {
+    text: "kaizen",
+    url: "https://kaizen-api-ju41.onrender.com/",
+    description:
+      "anime data API built over graphQL",
+    image: "https://i.ibb.co/5xJ18xT/Image-2.png",  
+    color: "#FFE3DC",
+    button: "#C97726",
+  },
+  {
+    text: "stonk",
+    url: "https://stonk.js.org/",
+    image: "https://i.ibb.co/kK1Hh1L/Group-2.png",
+    description:
+      "cli tool to monitor indian stocks",
+    color: "#EAFEF9",
+    button: "#32A37A",
+  },
  ]
 
  module.exports = projects   
