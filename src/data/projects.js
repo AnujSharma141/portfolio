1cd51f15d0b93c2d4c4070582f99918e862a8cbc
Anuj Sharma <19ucc109@lnmiit.ac.in>
2023-10-12 15:22:00 +0530

    update: project links!

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,10 +3,19 @@ const projects = [
     text: "reactsearch",
     url: "https://reactsearch.js.org/",
     description:
-      "npm package to create custom react searchbar",
+      "npm package to create custom react searchbar.",
     image: "https://i.ibb.co/h7q1vDt/Group-1.png",
     color: "#FFE3F4",
     button: "#AD29A4",
+  },
+  {
+    text: "dashboard",
+    url: "https://springbootreact.netlify.app/",
+    description:
+      "dashboard made with spring boot, react and postgreSQL.",
+    image: "https://i.ibb.co/VmsCrhd/Image-3.png",
+    color: "#E8EBFF",
+    button: "#2F5B8D",
   },
   {
     text: "tralo",
@@ -21,7 +30,7 @@ const projects = [
     text: "kaizen",
     url: "https://kaizenlink.tech/",
     description:
-      "anime data API built over graphQL",
+      "anime data API built over graphQL.",
     image: "https://i.ibb.co/5xJ18xT/Image-2.png",  
     color: "#FFE3DC",
     button: "#C97726",
@@ -31,7 +40,7 @@ const projects = [
     url: "https://stonk.js.org/",
     image: "https://i.ibb.co/kK1Hh1L/Group-2.png",
     description:
-      "cli tool to monitor indian stocks",
+      "cli tool to monitor indian stocks.",
     color: "#EAFEF9",
     button: "#32A37A",
   },
